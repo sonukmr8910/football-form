ada0174b54af84a80b56cac5c9e27527a3ef96ab
Sonu Kumar K <skumar@catalystone.com>
2022-02-07 16:35:12 +0530

    initial commit

---
+++ src/main/java/com/form/footballform/FootballFormApplication.java
@@ -0,0 +1,13 @@
+package com.form.footballform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/form/footballform/FootballFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.form.footballform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
