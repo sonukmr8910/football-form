285da1c24e8fde68d19b0771806d8ebd960cc6a5
Sonu Kumar K <skumar@catalystone.com>
2022-02-07 16:45:37 +0530

    created FootballController class

---
+++ src/main/java/com/form/footballform/controller/FootballController.java
@@ -0,0 +1,10 @@
+package com.form.footballform.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/football")
+public class FootballController {
+
+}
