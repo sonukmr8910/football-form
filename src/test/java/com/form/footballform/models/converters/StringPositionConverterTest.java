d65cc221d8928757875de5a7e69a05a7be3b5f00
Sonu Kumar K <skumar@catalystone.com>
2022-02-08 18:57:26 +0530

    added test cases for StringPositionConverter class

---
+++ src/test/java/com/form/footballform/models/converters/StringPositionConverterTest.java
@@ -0,0 +1,37 @@
+package com.form.footballform.models.converters;
+
+import com.form.footballform.models.Position;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringPositionConverterTest {
+
+    StringPositionConverter converter;
+
+    @BeforeEach
+    void setUp() {
+        converter = new StringPositionConverter();
+    }
+
+    @Test
+    void convertToDatabaseColumn() {
+        assertEquals("null-Striker", converter.convertToDatabaseColumn(List.of(new Position("Striker"))));
+        assertEquals("null-Goal Keeper", converter.convertToDatabaseColumn(List.of(new Position("Goal Keeper"))));
+        assertEquals("1-Striker", converter.convertToDatabaseColumn(List.of(new Position(1L, "Striker"))));
+        assertEquals("1-Goal Keeper", converter.convertToDatabaseColumn(List.of(new Position(1L, "Goal Keeper"))));
+        assertEquals("null-", converter.convertToDatabaseColumn(List.of(new Position(""))));
+        assertEquals("null-123", converter.convertToDatabaseColumn(List.of(new Position("123"))));
+        assertEquals("123-123", converter.convertToDatabaseColumn(List.of(new Position(123L, "123"))));
+        assertEquals("1-Striker;1-Striker;1-Striker;1-Striker;1-Striker", converter.convertToDatabaseColumn(List.of(
+                new Position(1L, "Striker"),
+                new Position(1L, "Striker"),
+                new Position(1L, "Striker"),
+                new Position(1L, "Striker"),
+                new Position(1L, "Striker")
+        )));
+    }
+}
