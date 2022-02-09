77f2f1ba4282ab2f4dd73de67975afb3568c9ca6
Sonu Kumar K <skumar@catalystone.com>
2022-02-09 10:24:00 +0530

    added FootballController with GetMapping

--- src/main/java/com/form/footballform/controller/FootballController.java
+++ src/main/java/com/form/footballform/controller/FootballController.java
@@ -1,15 +1,39 @@
 package com.form.footballform.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.form.footballform.models.Player;
+import com.form.footballform.models.Response;
+import com.form.footballform.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/football")
 public class FootballController {
+    private final PlayerService playerService;
 
-    @GetMapping
-    public String index() {
-        return "Index";
+    @Autowired
+    public FootballController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Response<Player>> getPlayer(@PathVariable("id") Long id) {
+        Player player = playerService.getPlayer(id);
+        if (player != null) {
+            Response<Player> response = new Response.ResponseBuilder<Player>()
+                    .setHttpStatusCode(HttpStatus.OK.value())
+                    .setData(player)
+                    .build();
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+
+        String errorMessage = String.format("User with id %d not found", id);
+        return new ResponseEntity<>(new Response.ResponseBuilder<Player>()
+                .setHttpStatusCode(HttpStatus.NOT_FOUND.value())
+                .setErrorMessage(errorMessage)
+                .build()
+                , HttpStatus.NOT_FOUND);
     }
 }
