44ad55b8c4eb448e22c717f3f2dd70a03ee841ca
Sonu Kumar K <skumar@catalystone.com>
2022-02-12 11:44:21 +0530

    added username existence check in PlayerRepository

--- src/main/java/com/form/footballform/repository/PlayerRepository.java
+++ src/main/java/com/form/footballform/repository/PlayerRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     Optional<Player> getPlayerById(Long id);
+    boolean existsByUserName(String userName);
 }
