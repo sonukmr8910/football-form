98ee48ab009421fe2740af2c342f19ab91cbfd06
Sonu Kumar K <skumar@catalystone.com>
2022-02-12 12:52:55 +0530

    added method to savePlayer using PlayerRequest

--- src/main/java/com/form/footballform/service/PlayerService.java
+++ src/main/java/com/form/footballform/service/PlayerService.java
@@ -1,18 +1,30 @@
 package com.form.footballform.service;
 
 import com.form.footballform.models.*;
+import com.form.footballform.models.request.PlayerRequest;
 import com.form.footballform.repository.PlayerRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class PlayerService {
     private final PlayerRepository playerRepository;
+    private final CityService cityService;
+    private final AddressService addressService;
+    private final AgeGroupService ageGroupService;
+    private final TeamService teamService;
+    private final PositionService positionService;
 
-    public PlayerService(PlayerRepository playerRepository) {
+    public PlayerService(PlayerRepository playerRepository, CityService cityService, AddressService addressService, AgeGroupService ageGroupService, TeamService teamService, PositionService positionService) {
         this.playerRepository = playerRepository;
+        this.cityService = cityService;
+        this.addressService = addressService;
+        this.ageGroupService = ageGroupService;
+        this.teamService = teamService;
+        this.positionService = positionService;
     }
 
     public Optional<Player> getPlayer(Long id) {
@@ -21,6 +33,39 @@ public class PlayerService {
 
     public boolean isUserNameAlreadyRegistered(String userName) {
         return playerRepository.existsByUserName(userName);
+    }
+
+    public Player savePlayer(final PlayerRequest request) {
+        try {
+            AgeGroup ageGroup = ageGroupService.getAgeGroup(request.getAgeGroup()).orElseThrow();
+            City city = cityService.getCity(request.getCity()).orElseThrow();
+            Address address = addressService.saveAddress(new Address(request.getAddress(), city, request.getPinCode()));
+            Team desiredTeam = teamService.getTeam(request.getDesiredTeam()).orElseThrow();
+
+            List<Long> posIds = request.getDesiredPositions();
+            List<Position> desiredPositions = new ArrayList<>();
+            for(Long posId : posIds) {
+                Optional<Position> position = positionService.getPosition(posId);
+                position.ifPresent(desiredPositions::add);
+            }
+
+            Player player = new Player(
+                    request.getUserName(),
+                    request.getFirstName(),
+                    request.getLastName(),
+                    request.getPhoneNumber(),
+                    request.getEmail(),
+                    ageGroup,
+                    address,
+                    desiredTeam,
+                    desiredPositions
+            );
+
+            return playerRepository.save(player);
+        }
+        catch (Exception e) {
+            return null;
+        }
     }
 
     public Player savePlayer(Player player) {
