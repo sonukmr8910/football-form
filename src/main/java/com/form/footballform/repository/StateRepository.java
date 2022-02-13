ca70ec79f407089065f21b06ce334a35e5725921
Sonu Kumar K <skumar@catalystone.com>
2022-02-13 18:44:20 +0530

    added getStatesByCountryId method signature

--- src/main/java/com/form/footballform/repository/StateRepository.java
+++ src/main/java/com/form/footballform/repository/StateRepository.java
@@ -4,9 +4,11 @@ import com.form.footballform.models.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StateRepository extends JpaRepository<State, Long> {
     Optional<State> getStateById(Long id);
+    List<State> getStatesByCountryId(Long countryId);
 }
