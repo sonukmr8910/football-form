22979e52f07a390a5a54f19319a8cd701719e9b4
Sonu Kumar K <skumar@catalystone.com>
2022-02-13 18:43:51 +0530

    added getCitysByStateId method signature

--- src/main/java/com/form/footballform/repository/CityRepository.java
+++ src/main/java/com/form/footballform/repository/CityRepository.java
@@ -4,9 +4,11 @@ import com.form.footballform.models.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
     Optional<City> getCityById(Long id);
+    List<City> getCitysByStateId(Long stateId);
 }
