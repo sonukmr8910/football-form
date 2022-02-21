402ce69bda27c11b165fa448353327bb005cbddb
Sonu Kumar K <skumar@catalystone.com>
2022-02-21 13:37:57 +0530

    added checkbox near email field

--- src/main/resources/public/js/init.js
+++ src/main/resources/public/js/init.js
@@ -5,6 +5,16 @@ var citySelectElement = document.getElementById('city');
 var ageGroupSelectElement = document.getElementById("age-group");
 var btnSubmit = document.getElementById('submit-btn');
 
+let emailTextBox = document.getElementById('email');
+let emailSwitch = document.getElementById('email-switch');
+emailSwitch.addEventListener('change', (event) => {
+   if(event.target.checked) {
+      emailTextBox.removeAttribute('disabled');
+   }
+   else {
+      emailTextBox.setAttribute('disabled', 'true')
+   }
+});
 
 function removeOptions(selectElement) {
     var i, L = selectElement.options.length - 1;
